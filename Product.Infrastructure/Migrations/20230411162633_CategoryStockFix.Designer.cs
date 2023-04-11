71d780434c419f84a1086f0d95cdf5a7407e8308
Serkan İnce <serkan.ince@zirveyazilim.net>
2023-04-11 20:32:16 +0300

    add support Product should have a minimum stock quantity in Category level

--- Product.Infrastructure/Migrations/20230411162633_CategoryStockFix.Designer.cs
+++ Product.Infrastructure/Migrations/20230411162633_CategoryStockFix.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Product.Infrastructure.Persistence;
@@ -11,9 +12,11 @@ using Product.Infrastructure.Persistence;
 namespace Product.Infrastructure.Migrations
 {
     [DbContext(typeof(ProductDBContext))]
-    partial class ProductDBContextModelSnapshot : ModelSnapshot
+    [Migration("20230411162633_CategoryStockFix")]
+    partial class CategoryStockFix
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
